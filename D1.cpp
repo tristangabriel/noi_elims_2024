2bb93539022e529fccd91d0103aa44eee8c2d5e3
tristangabriel <142296311+tristangabriel@users.noreply.github.com>
2024-03-03 12:19:09 +0800

    Add files via upload

--- D1.cpp
+++ D1.cpp
@@ -3,24 +3,27 @@
 
 using namespace std;
 
-long long int temp[500000];
-long long int arr[500000];
-long long int b[500001];
-long long int c[500001];
-const long long int zero = 0;
+typedef long long int lli;
+#define REP(i, n) for (int i=0;i<n;i++)
+
+lli temp[500000];
+lli arr[500000];
+lli b[500001];
+lli c[500001];
+const lli zero = 0;
 int main() {
     int t, n, k, num; cin >> t;
     while (t--) {
-        long long int ppcost=0, num;
+        lli ppcost=0, num;
         cin >> n >> k;
-        for (int i=0;i<n;i++) {
+        REP(i, n) {
             cin >> temp[i];
         }
-        for (int i=0;i<n;i++) {
+        REP(i, n) {
             cin >> num;
             arr[i] = temp[num-1];
         }
-        for (int i=0;i<n;i++) {
+        REP(i, n) {
             c[i+1] = c[i] + b[i];
             if (i>=k) {
                 c[i+1] -= b[i-k+1];
